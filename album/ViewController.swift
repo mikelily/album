3d9ef3909bf54742d009771cd8c825cd861976bc
mike01_lin <mike01_lin@tti.tv>
2022-03-28 23:18:34 +0800

    Initial Commit

---
+++ album/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  album
+//
+//  Created by 林華淵 on 2022/3/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
